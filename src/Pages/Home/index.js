cf3a085020407b54787a6d43163933a8193431d1
Jaydeep Vala <thesame@email.com>
2024-03-19 16:18:03 +0530

    Initial commit

---
+++ src/Pages/Home/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Wrapper } from "./Style";
+
+const Home = () => {
+  return (
+    <>
+      <Wrapper>
+        <section className="container-fluid">
+          <h1>Home</h1>
+        </section>
+      </Wrapper>
+    </>
+  );
+};
+
+export default Home;
---
+++ src/Pages/Products/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Wrapper } from "./style";
+
+const products = () => {
+  return (
+    <>
+      <Wrapper>
+        <section className="container-fluid main-product-page">
+          <h1>Product page </h1>
+        </section>
+      </Wrapper>
+    </>
+  );
+};
+
+export default products;
